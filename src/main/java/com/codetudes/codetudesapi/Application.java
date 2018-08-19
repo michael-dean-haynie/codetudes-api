09507abeb12e10e3b408d3f56b1242c3c02fbd5a
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2018-08-19 16:01:47 -0400

    Enable communication with ui

--- src/main/java/com/codetudes/codetudesapi/Application.java
+++ src/main/java/com/codetudes/codetudesapi/Application.java
@@ -1,9 +1,18 @@
 package com.codetudes.codetudesapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -13,7 +22,23 @@ public class Application {
     }
 
     @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(env.getProperty("cors.ui"));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+    @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+
 }
